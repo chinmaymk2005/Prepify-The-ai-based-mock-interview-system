241c15b3f3a0702ba23f623b560f2cb1c5b2b5d9
chinmaymk2005 <chinmaymk13@gmail.com>
2026-01-15 13:09:28 +0530

    basic set up ready

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {  
+  return (
+    <div>
+      <h1 className="text-center m-2 bg-blue-500 text-white p-4 rounded-lg"
+      >Prepify is working</h1>
+    </div>  
+  );
+}
+
+export default App;
